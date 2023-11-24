4db8c502529636e2ac2e99586cc4bcf428aa3b80
Dhanunjay <dhanunjayareddykurakula@gmail.com>
2023-11-24 23:17:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const patientRoute = require("./controller/patientRoute.js");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+mongoose.set("strictQuery",true); //Deprection Warning //To supress warning
+// mongoose.connect("mongodb+srv://test:12345@cluster0.bkk3fg5.mongodb.net/schooldb");
+mongoose.connect("mongodb://127.0.0.1:27017/appointment_booking");
+var db = mongoose.connection;
+db.on("open",()=>console.log("Connected to DB"));
+db.on("error",()=>console.log("Error occurred"));
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cors());
+app.use("/patientRoute",patientRoute);
+
+app.listen(3001,()=>{
+    console.log("Server started at 3001");
+})
