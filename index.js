08dec5ccd2e313441defc3212679cd84a5b8367d
Dhanunjay-369 <112246282+Dhanunjay-369@users.noreply.github.com>
2023-11-25 14:44:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const app = express();
 
 mongoose.set("strictQuery",true); //Deprection Warning //To supress warning
 // mongoose.connect("mongodb+srv://test:12345@cluster0.bkk3fg5.mongodb.net/schooldb");
-mongoose.connect("mongodb://127.0.0.1:27017/appointment_booking");
+// mongoose.connect("mongodb://127.0.0.1:27017/appointment_booking");
+mongoose.connect("mongodb+srv://dhanu369:369@test.gj5qniu.mongodb.net/");
 var db = mongoose.connection;
 db.on("open",()=>console.log("Connected to DB"));
 db.on("error",()=>console.log("Error occurred"));
